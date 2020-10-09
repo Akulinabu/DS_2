781e8c3ee8d291e4b9e11e736007a5ebf8aeec7e
Akulinabu <akulinabugaeva@mail.ru>
2020-10-09 20:13:35 +0900

    калькултор

--- 7.loops.py
+++ 7.loops.py
@@ -51,6 +51,64 @@ num = 0
 #     my_dict[[ind] =indx *10]
 
 #г***Генеротор списка ***
- 
- my_list = [num ** 2 for num in range (10) if num >3]
- print (my_list)
+#  my list = [num for num in range (10)]
+#  my_list = [num ** 2 for num in range (10) if num >3]
+#  print (my_list)
+
+# *** класс enumerate() ***
+
+date = ['a', 'b', 'c', 'd']
+
+# print(list (enumerate(date)))
+
+# for index, item in enumerate(date):
+#     print (index, item)
+
+
+#**** генератор словаря***
+
+# my_dict = {i : i for i in ['A', 'B', 'c', 'd'] }
+
+my_dict = {item.upper() : index *10 for index, item in enumerate(date) if index > 0 }
+
+
+# print (my_dict)
+
+# ***калькулятор с циклами ***
+
+while True:
+    cmd = None
+    # вывод чисел
+    num_list = []
+    for n in range (2):
+        num = int (input(f"Введите {n} число: "))
+        num_list.append(num)
+
+    # ввод команды
+    while True:
+        cmd = input ("Введите команду: ")
+        if cmd not in {"stop", "+", "-", "*", "/"}:
+            print("Вы ввели неправильную команду!!! :((((")
+            continue
+        else:
+            break
+    
+    # обработка выключения
+    if cmd == "stop":
+        print("До свидание!")
+        break
+    # вычисление
+    if cmd == "+":
+        res = num_list[0] + num_list[1]
+    elif cmd == "-":
+        res = num_list[0] - num_list[1]
+    elif cmd == "*":
+        res = num_list[0] * num_list[1]
+    elif cmd == "/":
+        res = num_list[0] / num_list[1]
+
+    if float is type (res):
+        #  вывод результата
+        print(f"Результат: {res:.4f}")
+    else:
+        print(f"Результат: {res:.4f}")
